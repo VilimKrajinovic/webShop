eec8e9568dbd2bf840d0505e6bfbdda03aca4909
vkrajinovic <vilim.krajinovic@infobip.com>
2019-02-13 09:56:14 +0100

    refactored code

--- src/main/java/com/vkraji/webShop/models/register/RegisterForm.java
+++ src/main/java/com/vkraji/webShop/models/register/RegisterForm.java
@@ -8,11 +8,11 @@ public class RegisterForm {
     private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
     private static final String EMAIL_MESSAGE = "{email.message}";
 
-    @NotBlank(message = RegisterForm.NOT_BLANK_MESSAGE)
-    @Email(message = RegisterForm.EMAIL_MESSAGE)
+    @NotBlank(message = NOT_BLANK_MESSAGE)
+    @Email(message = EMAIL_MESSAGE)
     private String email;
 
-    @NotBlank(message = RegisterForm.NOT_BLANK_MESSAGE)
+    @NotBlank(message = NOT_BLANK_MESSAGE)
     private String password;
 
     public String getEmail() {
